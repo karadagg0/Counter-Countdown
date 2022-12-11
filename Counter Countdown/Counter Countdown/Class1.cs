fdbe4482ace262920275e73ef9c114c92bb8f443
karadagg0 <104911016+karadagg0@users.noreply.github.com>
2022-12-11 16:46:27 +0300

    Counter Countdown
    Counter Countdown Alghorithm

---
+++ Counter Countdown/Counter Countdown/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Counter_Countdown
+{
+    public class Class1
+    {
+         if(timer && totaltime > 0)
+        {
+            totaltime -= Time.deltaTime;
+
+            minute = Mathf.FloorToInt(totaltime / 60);
+            second = Mathf.FloorToInt(totaltime % 60);
+            counter.text = string.Format("{0:00}  :  {1:00}", minute, second);
+    }
+        else
+        {
+            timer = false;
+        }
+    }
+}
